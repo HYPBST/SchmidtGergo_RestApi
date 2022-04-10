dfc58d58363ff574b994ed148be199cf47dbcf98
Gergő <s.gergo.az@gmail.com>
2022-04-10 13:24:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/schmidtgergo_restapi/InsertActivity.java
@@ -0,0 +1,6 @@
+package com.example.schmidtgergo_restapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InsertActivity  extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/schmidtgergo_restapi/ListResultActivity.java
@@ -0,0 +1,6 @@
+package com.example.schmidtgergo_restapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListResultActivity  extends AppCompatActivity {
+}
